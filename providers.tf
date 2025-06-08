516f1adc27bb2ff2536f2c4c01df4b638cce3caf
Sean Taylor <spttaylor@gmail.com>
2025-06-08 02:19:39 -0700

    Testing

--- providers.tf
+++ providers.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "github" {
-  app_auth {
-    id              = var.github_app_id
-    installation_id = var.github_app_installation_id
-    pem_file        = var.github_app_pem_file
-  }
+  app_auth {}
 }
