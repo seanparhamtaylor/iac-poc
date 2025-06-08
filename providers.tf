7aa1e92d88c98f4d8819a7c40f1c2972967e5d26
Sean Taylor <spttaylor@gmail.com>
2025-06-08 02:54:44 -0700

    Testing

--- providers.tf
+++ providers.tf
@@ -2,25 +2,8 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
     }
   }
 }
 
-provider "github" {
-  # app_auth {
-  #   id              = "1379247"
-  #   installation_id = "70341683"
-  #   pem_file = file("/Users/seantaylor/Workspace/iac-poc-project/iac-poc/spt-iac-bot.2025-06-07.private-key.pem")
-  # }
-}
-
-# Add this to enable debug logging
-provider "github" {
-  alias = "debug"
-  # app_auth {
-  #   id              = "1379247"
-  #   installation_id = "70341683"
-  #   pem_file        = file("/Users/seantaylor/Workspace/iac-poc-project/iac-poc/spt-iac-bot.2025-06-07.private-key.pem")
-  # }
-}
+provider "github" {}
