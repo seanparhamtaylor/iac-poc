c2bf7e65285d38b6f61795ce0e825bc55b55f39c
Sean Taylor <spttaylor@gmail.com>
2025-06-08 02:03:45 -0700

    Testing

--- env.sh
+++ env.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# Function to escape newlines and quotes for JSON
+escape_json() {
+    printf '%s' "$1" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g'
+}
+
 cat <<EOF
 {
   "GITHUB_APP_ID": "$GITHUB_APP_ID",
-  "GITHUB_APP_PEM_FILE": "$GITHUB_APP_PEM_FILE",
+  "GITHUB_APP_PEM_FILE": "$(escape_json "$GITHUB_APP_PEM_FILE")",
   "GITHUB_APP_INSTALLATION_ID": "$GITHUB_APP_INSTALLATION_ID"
 }
 EOF
