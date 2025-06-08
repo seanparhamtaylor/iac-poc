943d97d2ddb146dd01fe12b6ebd28e7b178972cd
Sean Taylor <spttaylor@gmail.com>
2025-06-08 02:05:37 -0700

    Testing

--- providers.tf
+++ providers.tf
@@ -11,6 +11,6 @@ provider "github" {
   app_auth {
     id              = data.external.env.result["GITHUB_APP_ID"]
     installation_id = data.external.env.result["GITHUB_APP_INSTALLATION_ID"]
-    pem_file        = data.external.env.result["GITHUB_APP_PEM_FILE"]
+    pem_file        = replace(data.external.env.result["GITHUB_APP_PEM_FILE"], "\\n", "\n")
   }
 }
